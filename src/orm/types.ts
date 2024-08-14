8866fb916e3532fc5b6e2cd96ba1816415d3bf8e
Sanay Nesargi <sanaynesargi@gmail.com>
2024-08-14 13:58:05 -0500

    Removed env

---
+++ src/orm/types.ts
@@ -0,0 +1,36 @@
+import {
+  ColumnType,
+  Generated,
+  Insertable,
+  Selectable,
+  Updateable,
+} from "kysely";
+
+export interface ClientsTable {
+  id: Generated<number>;
+  first_name: string;
+  last_name: string;
+  created_at: ColumnType<Date, string | undefined, never>;
+}
+
+export interface RecordStoreTable {
+  id: Generated<number>;
+  person_id: number;
+  s3_url: string;
+  created_at: ColumnType<Date, string | undefined, never>;
+}
+
+// The overall database interface that includes all the tables
+export interface Database {
+  clients: ClientsTable;
+  record_store: RecordStoreTable;
+}
+
+// Export different types to isolate functionality
+export type Client = Selectable<ClientsTable>;
+export type NewClient = Insertable<ClientsTable>;
+export type ClientUpdate = Updateable<ClientsTable>;
+
+export type Record = Selectable<RecordStoreTable>;
+export type NewRecord = Insertable<RecordStoreTable>;
+export type RecordUpdate = Updateable<RecordStoreTable>;
