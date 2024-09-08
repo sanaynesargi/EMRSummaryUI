a85f5f9c645a828bf96ab78bdf85866755ead4ee
Sanay Nesargi <sanaynesargi@gmail.com>
2024-09-08 17:52:49 -0500

    Fixed export option

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   // (Optional) Export as a static site
   // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
-  output: "export", // Feel free to modify/remove this option
-
   // Override the default webpack configuration
   webpack: (config) => {
     // See https://webpack.js.org/configuration/resolve/#resolvealias
