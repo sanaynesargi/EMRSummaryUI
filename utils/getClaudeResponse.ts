597ba010a32a403a5fffee830befae52aa6d64fc
Sanay Nesargi <sanaynesargi@gmail.com>
2024-09-08 17:24:02 -0500

    didy final fix 2

--- utils/getClaudeResponse.ts
+++ utils/getClaudeResponse.ts
@@ -1,30 +1,15 @@
-// import Anthropic from "@anthropic-ai/sdk";
-
-// const anthropic = new Anthropic();
-
-// // export const getClaudeResponse = async (
-// //   systemMessage: string,
-// //   isLargeMessage: boolean
-// // ) => {
-// //   const msg = await anthropic.messages.create({
-// //     model: isLargeMessage
-// //       ? "claude-3-haiku-20240307"
-// //       : "claude-3-5-sonnet-20240620",
-// //     messages: [{ role: "user", content: systemMessage }],
-// //     max_tokens: 1000,
-// //   });
-
-// //   const content: any = msg.content[0];
-
-// //   return content.text;
-// // };
 import Anthropic from "@anthropic-ai/sdk";
 
 const anthropic = new Anthropic();
 
-export const getClaudeResponse = async (systemMessage: string) => {
+export const getClaudeResponse = async (
+  systemMessage: string,
+  isLargeMessage: boolean
+) => {
   const msg = await anthropic.messages.create({
-    model: "claude-3-5-sonnet-20240620",
+    model: isLargeMessage
+      ? "claude-3-haiku-20240307"
+      : "claude-3-5-sonnet-20240620",
     messages: [{ role: "user", content: systemMessage }],
     max_tokens: 1000,
   });
